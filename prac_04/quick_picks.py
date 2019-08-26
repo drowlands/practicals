07768e9e67e9e1e0612db2fd58076e11dc4d69bb
Daniel R <drowlands52@gmail.com>
2019-08-27 00:32:37 +1000

    Fix maximum print

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,12 +1,21 @@
 import random
 
+MAXIMUM_VALUE = 1
+MINIMUM_VALUE = 45
+
 def main():
-    picks = int(input("How many quick-picks? "))
-    for row_counter in range(picks):
-        for column_counter in range(picks):
-            row =
 
-
-
+    pick_count = int(input("How many picks do you want? "))
+    for pick in range(pick_count):
+        # Generate line
+        # line = ""
+        numbers_in_line = []
+        while len(numbers_in_line) < 6:
+            new_number = random.randint(1, 45)
+            if new_number not in numbers_in_line:
+                # print("Hit")
+                numbers_in_line.append(str(new_number) + "\t")
+        line = "".join(numbers_in_line)
+        print(line)
 
 main()
