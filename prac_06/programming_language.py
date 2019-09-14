8a01fccbb94c2492020194f59b7abb9a8b8addaf
Daniel R <drowlands52@gmail.com>
2019-09-14 23:14:54 +1000

    Add functionality

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,3 +1,12 @@
 
 class ProgrammingLanguage:
     def __init__(self, typing, reflection, year):
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if (str(self.typing).lower() == "dynamic"):
+            return True
+        else:
+            return False
