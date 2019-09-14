6d34025805fedf830452489942bf66857dd73ffd
Daniel R <drowlands52@gmail.com>
2019-09-14 23:03:02 +1000

    Initial commit

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -15,5 +15,11 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
+    limo = Car(100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo)
+
 
 main()
