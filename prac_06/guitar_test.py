28b763c07fb1507e64ceb2d9bf9bcf1cb4961168
Daniel R <drowlands52@gmail.com>
2019-09-14 23:36:59 +1000

    Create file

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,7 @@
+from prac_06.guitar import Guitar
+
+def main():
+    first_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    second_guitar = Guitar("Another Guitar", 2012, 12345.67)
+
+    print()
