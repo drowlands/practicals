9e5cc2c864e356356d06cf98749b6480d3dbc664
Daniel R <drowlands52@gmail.com>
2019-09-14 23:18:13 +1000

    Add __str__ to class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,6 +6,9 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        return ("{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year))
+
     def is_dynamic(self):
         if (str(self.typing).lower() == "dynamic"):
             return True
