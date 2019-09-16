93f69ac66028334aaa8ec9f3379d419f4b6a610a
Daniel R <drowlands52@gmail.com>
2019-09-16 13:51:42 +1000

    Create file.

---
+++ prac_06/car_simulator.py
@@ -0,0 +1,13 @@
+from prac_06.car import Car
+
+def main():
+    print("Let's drive!")
+    new_name = input("Enter your car name: ")
+    new_car = Car(new_name, 100)
+
+
+def menu():
+    return
+
+
+main()
