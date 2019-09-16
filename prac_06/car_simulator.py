5eda67607234dbf8f32d4bcb7b6634e9176c7abb
Daniel R <drowlands52@gmail.com>
2019-09-16 14:07:14 +1000

    Implement drive function.

--- prac_06/car_simulator.py
+++ prac_06/car_simulator.py
@@ -2,12 +2,25 @@ from prac_06.car import Car
 
 def main():
     print("Let's drive!")
-    new_name = input("Enter your car name: ")
+    # new_name = input("Enter your car name: ")
+    # new_car = Car(new_name, 100)
+
     new_car = Car(new_name, 100)
 
+    print(new_car)
 
-def menu():
-    return
+
+def menu(car):
+    new_input = input("Enter your choice: ")
+    drive(car)
+def drive(car):
+    while True:
+        distance = int(input("How many km do you wish to drive? "))
+        if distance < 0:
+            print("Distance must be >= 0")
+        else:
+            car.drive(distance)
+
 
 
 main()
