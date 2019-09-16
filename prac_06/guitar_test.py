21e079e937433630a909d4d70925ad0bfd5437ff
Daniel R <drowlands52@gmail.com>
2019-09-16 11:06:51 +1000

    Complete exercise.

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -4,11 +4,24 @@ def main():
     first_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
     second_guitar = Guitar("Another Guitar", 2012, 12345.67)
 
-    print("{} get_age() - Expected {}. Got {}".format(first_guitar.name, first_guitar.year, first_guitar.get_age()))
-    print("{} get_age() - Expected {}. Got {}".format(second_guitar.name, second_guitar.year, second_guitar.get_age()))
+    print("{} get_age() - Expected {}. Got {}".format(first_guitar.name, 2019 - first_guitar.year, first_guitar.get_age()))
+    print("{} get_age() - Expected {}. Got {}".format(second_guitar.name, 2019 - second_guitar.year, second_guitar.get_age()))
 
-    print("{} is_vintage() - Expected {vintage}. Got {}".format(first_guitar.name, vintage=True if (2019 - first_guitar.year) > 50 else False, first_guitar.is_vintage()))
-    print("{} is_vintage() - Expected {vintage}. Got {}".format(second_guitar.name, vintage=True if (2019 - second_guitar.year) > 50 else False, second_guitar.is_vintage()))
+    if (2019 - first_guitar.year) > 50:
+        vintage_first = True
+    else:
+        vintage_first = False
+
+    if (2019 - second_guitar.year) > 50:
+        vintage_second = True
+    else:
+        vintage_second = False
+
+
+    print("{} is_vintage() - Expected {vintage}. Got {check}".format(first_guitar.name, vintage=vintage_first, check=first_guitar.is_vintage()))
+    print("{} is_vintage() - Expected {vintage}. Got {check}".format(second_guitar.name, vintage=vintage_second, check=second_guitar.is_vintage()))
 
     first_guitar.get_age()
     first_guitar.is_vintage()
+
+main()
