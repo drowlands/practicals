d8bdab5077e8d56edd25127b89a52d770366648b
Daniel R <drowlands52@gmail.com>
2019-09-16 23:35:52 +1000

    Implement menu function.

--- prac_06/car_simulator.py
+++ prac_06/car_simulator.py
@@ -2,22 +2,32 @@ from prac_06.car import Car
 
 def main():
     print("Let's drive!")
-    # new_name = input("Enter your car name: ")
-    # new_car = Car(new_name, 100)
+    new_name = input("Enter your car name: ")
+    new_car = Car(new_name, 100)
 
-    new_car = Car("The Bomb", 100)
+    # new_car = Car("The Bomb", 100)
 
     # print(new_car)
     menu(new_car)
 
 def menu(car):
-    # new_input = input("Enter your choice: ")
-    drive(car)
-    refuel(car)
+    print("Menu:\nd) drive\nr) refuel\nq) quit")
+    new_input = input("Enter your choice: ")
+    while True:
+        if new_input.lower() == "d":
+            drive(car)
+        elif new_input.lower() == "r":
+            refuel(car)
+        elif new_input.lower() == "q":
+            print("Good bye {}'s driver.".format(car.name))
+            return
+        else:
+            print("Invalid choice")
 
 
 def drive(car):
-    while True:
+    check = True
+    while check:
         distance = int(input("How many km do you wish to drive? "))
         if distance < 0:
             print("Distance must be >= 0")
@@ -27,9 +37,10 @@ def drive(car):
             odometer_after = car.odometer
             if car.fuel > 0:
                 print("The car drove {}km.".format(distance))
+                check = False
             else:
                 print("The car drove {}km and ran out of fuel.".format(odometer_after - odometer_before))
-            return
+                check = False
 
 
 def refuel(car):
